c616350eb2dbee8a6cd400a5318184aa3951cd7e
prashant patel <prashantpatel487330@gmail.com>
2026-02-19 23:48:16 +0530

    search

--- frontend/src/Pages/Search.jsx
+++ frontend/src/Pages/Search.jsx
@@ -1,4 +1,20 @@
+import axios from "axios";
+import { useState } from "react";
 const Search=()=>{
+    const[empno, setEmpno]=useState("");
+    const[mydata, setMydata]=useState([]);
+    const handleSubmit=async()=>{
+        let api=`http://localhost:8000/employee/search/?empno=${empno}`;
+        const response=await axios.get(api);
+        console.log(response.data);
+        setMydata(response.data);
+    }
+    const ans=mydata.map((key)=>{
+        return(
+            <>
+            </>
+        )
+    })
     return(
         <>
         <h1>Search</h1>
